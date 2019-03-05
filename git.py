44d3a98b94583d8d51857b3f9b5e8e202a4c7136
hoholam0112 <hoholam0112@gmail.com>
2019-03-05 18:13:09 +0900

    updated git.py

--- git.py
+++ git.py
@@ -2,7 +2,8 @@ import subprocess
 
 def main():
     subprocess.call('sudo git config --global core.editor \'vim\'', shell=True)
-
+    subprocess.call('sudo git config --global user.email \'hoholam0112@gmail.com\'', shell=True)
+    subprocess.call('sudo git config --global user.name \'hoholam0112\'', shell=True)
 
 if __name__ == '__main__':
     main()
